3fa0b4f0bb34745f871770dc64fd42f703ea90ac
philipe_kraken <philipeacampos@hotmail.com>
2022-10-26 20:42:28 -0300

    Refactored

---
+++ src/data/index.ts
@@ -0,0 +1,33 @@
+export const db = [
+  {
+    name: 'Coca Cola',
+    brand: 'Coca Cola',
+    price: 7.49,
+    unity: 1,
+    quantity: '02 litros',
+    promotion: false,
+    image: '../../assets/drinkCocaCola.jpg',
+    location: {
+      hall: 3,
+      rack: 'left',
+      column: 1,
+      sector: 'drinks'
+    }
+  },
+  {
+    name: 'Integral',
+    brand: 'Piracanjuba',
+    price: 4.49,
+    unity: 1,
+    quantity: '01 litro',
+    promotion: true,
+    image: '../../assets/drinkLeite.jpg',
+    location: {
+      hall: 3,
+      rack: 'left',
+      column: 7,
+      sector: 'drinks'
+    }
+  },
+  
+]
